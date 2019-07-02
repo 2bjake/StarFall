7789e5f637140e8099afc62b2d27c2f3d9fd46d3
Jake <jakef@fastmail.com>
2019-07-02 14:03:57 -0500

    clean up generated code

--- StarFall/GameViewController.swift
+++ StarFall/GameViewController.swift
@@ -26,21 +26,6 @@ class GameViewController: UIViewController {
             }
             
             view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
         }
     }
 
