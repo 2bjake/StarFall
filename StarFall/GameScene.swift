77dda05d91e487005e55bdd9c1c4e0d6a5ce76e8
Jake <jakef@fastmail.com>
2019-07-02 19:53:26 -0500

    clamp location so that star isn't placed half off the screen

--- StarFall/GameScene.swift
+++ StarFall/GameScene.swift
@@ -17,6 +17,9 @@ class GameScene: SKScene {
     private let starTexture = SKTexture(imageNamed: "star")
     private var starBuffer = FixedSizeBuffer<SKNode>(size: 50)
 
+    private lazy var starLength = CGFloat(size.width / 12)
+    private lazy var starSize = CGSize(width: starLength, height: starLength)
+
     override func didMove(to view: SKView) {
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
         motionManager.startDeviceMotionUpdates(using: .xArbitraryZVertical, to: motionQueue) { [weak physicsWorld] motion, error in
@@ -27,18 +30,37 @@ class GameScene: SKScene {
     }
 
     private func makeStarAt(_ position: CGPoint) -> SKNode {
-        let size = CGSize(width: 75, height: 75)
-        let starNode = SKSpriteNode(texture: starTexture, color: .yellow, size: size)
+        let starNode = SKSpriteNode(texture: starTexture, color: .yellow, size: starSize)
         starNode.colorBlendFactor = 0.75
         starNode.position = position
-        starNode.physicsBody = SKPhysicsBody(texture: starTexture, size: size)
+        starNode.physicsBody = SKPhysicsBody(texture: starTexture, size: starSize)
         return starNode
+    }
+
+    func clampLocationForTouch(_ touch: UITouch) -> CGPoint {
+        var location = touch.location(in: self)
+
+        func clamp(_ value: inout CGFloat, max: CGFloat) {
+            let halfLength = starLength / 2
+            if value < halfLength {
+                value = halfLength
+            } else if value > max - halfLength {
+                value =  max - halfLength
+            }
+        }
+
+        clamp(&location.x, max: size.width)
+        clamp(&location.y, max: size.height)
+        return location
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
 
-        let newStar = makeStarAt(touch.location(in: self))
+        let location = clampLocationForTouch(touch)
+        guard nodes(at: location).isEmpty else { return } //clicked on star, not adding another
+
+        let newStar = makeStarAt(location)
         if let oldStar = starBuffer.replaceAppend(newStar) {
             oldStar.removeFromParent()
         }
